cd68a25e10834a3040b83ccb880743ae711b4597
Bikram Kashyap <kashyap.bikram@gmail.com>
2021-06-06 16:08:34 -0400

    Initial project

---
+++ src/main/java/person/biki/springmicroservices/msscbreweryclient/MsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package person.biki.springmicroservices.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/person/biki/springmicroservices/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package person.biki.springmicroservices.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
