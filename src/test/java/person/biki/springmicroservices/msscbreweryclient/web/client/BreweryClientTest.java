14cbb39cd65d720194ffefed6daae504e04cac3a
Bikram Kashyap <kashyap.bikram@gmail.com>
2021-06-06 18:39:51 -0400

    Added client to get beer

---
+++ src/test/java/person/biki/springmicroservices/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,23 @@
+package person.biki.springmicroservices.msscbreweryclient.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import person.biki.springmicroservices.msscbreweryclient.web.model.BeerDto;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}
